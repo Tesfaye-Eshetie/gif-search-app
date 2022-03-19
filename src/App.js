5f55614a721c3a9bcdc62566303274650b81bf1f
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-03-18 20:25:56 -0400

    added search form and its functionality

--- src/App.js
+++ src/App.js
@@ -4,29 +4,26 @@ import axios from 'axios';
 import SearchForm from './components/SearchForm';
 import GifList from './components/GifList';
 
-
 export default function App() {
   const [gifs, setGifs] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-      const {data} = await axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC');
+  useEffect(() => performSearch, []);
+
+  const performSearch = async(query)=> {
+    try {
+      const {data} = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`);
       setGifs(data.data);
-      console.log(data.data);
-      } catch (err) {
-          console.log('Error fetching and parsing data', err);
-        }
-    }
-      fetchData();
-  }, []);
+    } catch (err) {
+        console.log('Error fetching and parsing data', err);
+      }
+  }
 
   return (
     <div>
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">Gif Search App</h1>
-          <SearchForm />      
+          <SearchForm onSearch={performSearch} />      
         </div>   
       </div>    
       <div className="main-content">
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function SearchForm() {
+export default function SearchForm({ onSearch }) {
+  const [searchText, setSearchText] = useState("");
+ 
+  const onSearchChange = e => {
+    setSearchText(e.target.value);
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSearch(searchText);
+    e.currentTarget.reset();
+  }
+  
   return (
-    <div>SearchForm</div>
-  )
+    <form className="search-form" onSubmit={handleSubmit} >
+      <label className="is-hidden" htmlFor="search">Search</label>
+      <input type="search" 
+              onChange={onSearchChange}
+              name="search" 
+              placeholder="Search..." />
+      <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
+    </form>      
+  );
 }
